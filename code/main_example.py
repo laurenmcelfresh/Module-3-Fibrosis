aa4989989f65661dfbcfc8e8dcbe0adfa1b09fa5
laurenmcelfresh <pkd9wt@virginia.edu>
2026-03-19 13:19:05 -0400

    Update main_example.py

--- code/main_example.py
+++ code/main_example.py
@@ -10,12 +10,12 @@ import pandas as pd
 # Load the images you want to analyze
 
 filenames = [
-    r"../images/MASK_SK658 Llobe ch010039.jpg",
-    r"../images/MASK_SK658 Slobe ch010066.jpg",
-    r"../images/MASK_SK658 Slobe ch010147.jpg",
-    r"../images/MASK_SK658 Slobe ch010110.jpg",
-    r"../images/MASK_SK658 Slobe ch010130.jpg",
-    r"../images/MASK_SK658 Slobe ch010114.jpg",
+    r"C:\Users\student\Documents\GitHub\Module-3-Fibrosis\images\MASK_Sk658 Llobe ch010031.jpg",
+    r"C:\Users\student\Documents\GitHub\Module-3-Fibrosis\images\MASK_Sk658 Llobe ch010032.jpg",
+    r"C:\Users\student\Documents\GitHub\Module-3-Fibrosis\images\MASK_Sk658 Llobe ch010033.jpg",
+    r"C:\Users\student\Documents\GitHub\Module-3-Fibrosis\images\MASK_Sk658 Llobe ch010034.jpg",
+    r"C:\Users\student\Documents\GitHub\Module-3-Fibrosis\images\MASK_Sk658 Llobe ch010035.jpg",
+    r"C:\Users\student\Documents\GitHub\Module-3-Fibrosis\images\MASK_Sk658 Llobe ch010036.jpg",
 ]
 
 # Enter the depth of each image (in the same order that the images are listed above; you can find these in the .csv file provided to you which is tilted: "Filenames and Depths for Students")
